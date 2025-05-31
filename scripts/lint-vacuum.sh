5895b7b748b6a39666ddfa0ece6fcd5d003adfd1
Robbie Capps <rocapp@gmail.com>
2025-05-31 04:12:22 -0700

    scripts/lint-vacuum: added -d (detailed output) flag by default

--- scripts/lint-vacuum.sh
+++ scripts/lint-vacuum.sh
@@ -3,5 +3,5 @@ set -e
 timestamp=$(date +"%Y%m%d-%H%M%S")
 logfile="./logs/lint-output-$timestamp.log"
 mkdir -p ./logs
-vacuum lint ./openapi.json | tee "$logfile"
+vacuum lint -d ./openapi.json | tee "$logfile"
 
